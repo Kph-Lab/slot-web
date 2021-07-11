c4cbda4dfa35d6a7a5dac5bb80f868c55fe1638a
Potewo <potewo@protonmail.com>
2021-07-11 16:26:42 +0900

    画像のスクロール

--- main.ts
+++ main.ts
@@ -1,19 +1,36 @@
 import p5 from "p5";
+//@ts-ignore
+import Imgs from "./assets/images/sample/*.png";
 
 const sketch = (p: p5) => {
   let x = 50;
   let button: p5.Element;
+  let loadedImages: p5.Image[] = [];
+  const imgW = 100;
+  const imgH = 100;
+  for (let img in Imgs) {
+    loadedImages.push(p.loadImage(Imgs[img]));
+  }
+  let imgsY: Array<number> = Array(loadedImages.length);
+  for (let i = 0; i < imgsY.length; i++) {
+    imgsY[i] = 100 * i;
+  }
+  let v = 10;
+  let a = 0;
   p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
     button = p.createSlider(0, 100, x, 1);
-
-    p.createCanvas(400, 400);
   };
 
   p.draw = () => {
-    let v = button.value();
-    let vn = toNum(v, 0);
     p.background(220);
-    p.ellipse(50-vn, 100, 10, 80);
+    for (let i = 0; i < 6; i++) {
+      imgsY[i] += v;
+      if (imgsY[i] > 400) {
+        imgsY[i] -= 100*6;
+      }
+      p.image(loadedImages[i], 0, imgsY[i], imgW, imgH);
+    }
   };
 
   const toNum = (arg: any, d: number):number => {
