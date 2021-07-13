5875d44657c0e5ae1f6ba9a284cee7417b915d98
Potewo <potewo@protonmail.com>
2021-07-13 21:55:30 +0900

    周りの電灯の装飾を追加

--- main.ts
+++ main.ts
@@ -29,7 +29,45 @@ const sketch = (p: p5) => {
       if (imgsY[i] > 400) {
         imgsY[i] -= 100*6;
       }
-      p.image(loadedImages[i], 0, imgsY[i], imgW, imgH);
+      p.image(loadedImages[i], 200, imgsY[i], imgW, imgH);
+    }
+    drawCircles();
+  };
+
+  const drawCircles = () => {
+    p.noStroke();
+    let yellow = p.color("#f2d15b");
+    let count = Math.round(p.frameCount / 9) % 34;
+    // left
+    for (let i = 0; i < 12; i++) {
+      if (count == i) {
+        p.fill(yellow);
+      } else {
+        p.fill(255, 255, 255);
+      }
+      p.circle(40, 40+75*(12 - i), 40);
+    }
+
+    // top
+    count -= 12;
+    for (let i = 0; i < 10; i++) {
+      if (count == i) {
+        p.fill(yellow);
+      } else {
+        p.fill(255, 255, 255);
+      }
+      p.circle(40+75*i, 40, 40);
+    }
+
+    // right
+    count -= 10;
+    for (let i = 0; i < 12; i++) {
+      if (count == i) {
+        p.fill(yellow);
+      } else {
+        p.fill(255, 255, 255);
+      }
+      p.circle(40+75*9, 40+75+75*i, 40);
     }
   };
 
